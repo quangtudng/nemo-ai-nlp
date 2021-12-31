5c7b2db092a4659782ba3f3fb287da2425a43875
Quang Tu <quangtupct@gmail.com>
2022-01-01 01:14:33 +0700

    feat: init rasa structure

---
+++ tokenizer/vi_tokenizer.py
@@ -0,0 +1,9 @@
+from typing import List, Text
+from rasa.nlu.tokenizers.tokenizer import Token, Tokenizer
+from rasa.shared.nlu.training_data.message import Message
+from underthesea import word_tokenize
+class VietnameseTokenizer(Tokenizer):
+    def tokenize(self, message: Message, attribute: Text) -> List[Token]:
+        text = message.get(attribute)
+        words = word_tokenize(text)
+        return self._convert_words_to_tokens(words, text)
