42faca33fb95241e46c66471c61b5edd8975a1ad
Quang Tu <quangtupct@gmail.com>
2022-01-09 19:34:54 +0700

    chore: updated shell

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/bash
-source "./venv/bin/activate"
-rasa run --enable-api --auth-token xnLI0ajdlm3PmF5RX4w=
+source "./venv/bin/activate" && rasa run --enable-api --auth-token xnLI0ajdlm3PmF5RX4w=
