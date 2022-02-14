6f28998bc9e9eeb99d99386109f8a28389c8e1ca
Quang Tu <quangtupct@gmail.com>
2022-02-14 22:37:51 +0700

    chore: updated data

--- tokenizer/vi_tokenizer.py
+++ tokenizer/vi_tokenizer.py
@@ -5,5 +5,6 @@ from underthesea import word_tokenize
 class VietnameseTokenizer(Tokenizer):
     def tokenize(self, message: Message, attribute: Text) -> List[Token]:
         text = message.get(attribute)
+        text = text.lower()
         words = word_tokenize(text)
         return self._convert_words_to_tokens(words, text)
